240a07c112f81bb3e12e1155d9bcb16e22153de7
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-12-01 08:59:29 -0200

    chore(coverage): Add simplecov gem and configuration
    closes #2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'rspec'
+require 'byebug'
+
+if ENV['coverage'] == 'on'
+  require 'simplecov'
+  SimpleCov.start do
+    add_group 'lib', 'lib'
+    minimum_coverage 100
+  end
+end
+
 require 'ticketfy/cli'
